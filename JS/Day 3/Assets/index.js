748d769c0614559106b186d0b0d77e49f8e81448
Your Name <your-email@example.com>
2026-09-11 12:33:35 +0530

    Assignment3

---
+++ JS/Day 3/Assets/index.js
@@ -0,0 +1,100 @@
+// function add(a,b){
+//     return a+b;
+// }
+// let result=add(20,30);
+// let final=result*2;
+// console.log(final);
+
+//default function
+
+// function  defaul(name="dinesh")
+// {
+//     console.log("hello "+name);
+// }
+// defaul();
+
+// function expression
+
+// let add= function(a,b){
+//     return a+b;
+// };
+// console.log(add(10,20));
+
+// let one=function(j,o){
+
+//     return j+o;
+// }
+
+// console.log(one(10,20));
+
+
+//arrow function
+// const add=(a,b)=>{
+//     return a+b;
+// };
+// console.log(add(10,20));
+// const sub=(a,b)=>a-b;
+// console.log(sub(10,20));
+
+// arrow with one parameter
+
+// const square=(num)=>{
+//     return num* num;
+// };
+// console.log(square(10));
+
+// const square= num => num*num;
+// console.log(square(2));
+
+// function prin(doo){
+//     console.log(doo);
+// }
+// prin("hello javascript")
+
+// const printt=(word)=>word;
+// console.log(printt(hello));
+
+//call back function
+//without parameter
+
+// function students(){
+//     console.log("welcome");
+    
+// }
+// function staff(){
+//     console.log("Taking class");
+    
+// }
+
+// function school(programs){
+//     programs();
+// }
+// school(students);
+// school(staff);
+
+//with parameters
+// function bike(model){
+//     console.log("models "+model);
+// }
+
+// function sales(motors){
+//     motors("fz v1");
+// }
+// sales(bike);
+
+//setTimeout
+//anonymous
+// setTimeout(function (){
+//     console.log("hello");
+    
+// },1000);
+// //normal function
+// setTimeout(function day(){
+//     console.log("friday");
+    
+// },2000)
+// //Arrow
+// setTimeout(()=>{
+//     console.log("developed");
+    
+// },3000);
